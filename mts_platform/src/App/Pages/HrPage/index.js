a7c77d00d88262ebe82ab74c6ce66c62b6421e39
Yekaterina Kovylina <katuha.p97@gmail.com>
2019-09-28 12:06:01 +0300

    Added folder

---
+++ mts_platform/src/App/Pages/HrPage/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class HrPage extends Component {
+
+    render() {
+        return (
+            <div>
+                Hr page
+            </div>
+        );
+    }
+}
+
+export default HrPage;
