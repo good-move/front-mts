8f6c3c596ecf8ab1e1f1c3d679d94755ccc7b380
Alina Filyushina <filyushina.alina@gmail.com>
2019-09-28 20:21:16 +0700

    Fixed register

--- mts_platform/src/App/App.js
+++ mts_platform/src/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import EmployersPage from './Pages/EmployerPage';
-import HRPage from './Pages/hrPage';
+import HRPage from './Pages/HrPage';
 import MainPage from './Pages/MainPage';
 import EmployerResultPage from './Pages/Result/EmployerResultPage';
 import HrResult from './Pages/Result/HrResultPage';
