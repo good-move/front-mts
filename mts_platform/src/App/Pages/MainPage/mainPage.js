7c561210bc2ee83e682bb5dc2bd4aeb5455a7464
Yekaterina Kovylina <katuha.p97@gmail.com>
2019-09-28 15:34:55 +0300

    styling

--- mts_platform/src/App/Pages/MainPage/mainPage.js
+++ mts_platform/src/App/Pages/MainPage/mainPage.js
@@ -8,6 +8,12 @@ class MainPage extends Component {
         return (
             <div className="mainPage">
                 <Header/>
+
+                <div>
+                    <h1>
+                        Привет! Нужна помощь в поиске вакансии или подборе персонала? Я помогу, просто выбери подходящий режим!
+                    </h1>
+                </div>
             </div>
         );
     }
