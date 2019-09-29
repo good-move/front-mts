bc4593752f24b98481d83f31167bb06d07bbc81b
Yekaterina Kovylina <katuha.p97@gmail.com>
2019-09-29 03:12:05 +0300

    navigation from main page

--- mts_platform/src/App/Pages/MainPage/index.js
+++ mts_platform/src/App/Pages/MainPage/index.js
@@ -3,15 +3,26 @@ import Header from '../../Components/Header/header';
 import './component.css';
 
 class MainPage extends Component {
+    onButtonClickHandler = (path) => {
+        this.props.history.push(`${path}`);
+    };
+
     render() {
         return (
             <div className="mainPage">
                 <Header/>
 
-                <div>
-                    <h1>
-                        Привет! Нужна помощь в поиске вакансии или подборе персонала? Я помогу, просто выбери подходящий режим!
+                <div className="mainPageWrapper">
+                    <h1 className="mainHeader">
+                        <span>
+                            Привет!
+                        </span>
+                        Нужна помощь в поиске вакансии или подборе персонала? Я помогу, просто выбери подходящий режим!
                     </h1>
+                    <div className="mainPageButtonsWrapper">
+                        <button onClick={() => this.onButtonClickHandler('/hr')}>HR</button>
+                        <button onClick={() => this.onButtonClickHandler('/employers')}>Employer</button>
+                    </div>
                 </div>
             </div>
         );
