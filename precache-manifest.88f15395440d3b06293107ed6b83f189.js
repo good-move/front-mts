1c6a207a45c12aa10c5a70e3decfc6e382ecc648
a.boltava <goodmoveab@gmail.com>
2019-10-05 13:11:43 +0300

    gh pages

---
+++ precache-manifest.88f15395440d3b06293107ed6b83f189.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a539069aed2f1773d0f1b5b3d2f8c934",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65e603011c8bedc7d325",
+    "url": "/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "4aff30923d55c08c61b1",
+    "url": "/static/css/main.4d827ffb.chunk.css"
+  },
+  {
+    "revision": "65e603011c8bedc7d325",
+    "url": "/static/js/2.8dd9a7d3.chunk.js"
+  },
+  {
+    "revision": "4aff30923d55c08c61b1",
+    "url": "/static/js/main.796fc30d.chunk.js"
+  },
+  {
+    "revision": "a452c7c5f6ddd3edfcd2",
+    "url": "/static/js/runtime-main.39ee8089.js"
+  }
+]);
